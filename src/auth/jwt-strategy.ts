ca57c2a17a311050e121ef7fe975bbb8923df245
qiancheng.wang <qiancheng.wang@accenture.com>
2021-04-10 14:51:31 +1000

    feat(config): feature - config for deploy to AWS

--- src/auth/jwt-strategy.ts
+++ src/auth/jwt-strategy.ts
@@ -4,6 +4,9 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Strategy, ExtractJwt } from 'passport-jwt';
 import { User } from './auth.entity';
 import { UserRepository } from './auth.repository';
+import * as config from 'config';
+
+const jwtConfig = config.get('jwt');
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -13,7 +16,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: 'topSecret51',
+      secretOrKey: process.env.JWT_SECRET || jwtConfig.secret,
     });
   }
 
